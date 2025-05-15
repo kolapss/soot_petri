37ab025d1e78d4beea065121b94568a6e441bb7c
alexey <alekceilaptev@gmail.com>
2025-05-15 19:18:32 +0300

    add inhibitor arcs

--- src/main/java/com/kolaps/PTExtension.java
+++ src/main/java/com/kolaps/PTExtension.java
@@ -14,55 +14,54 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Map;
 
 public class PTExtension {
 
     private static ArrayList<String> inhibitorArcs = new ArrayList<>();
 
-    public static void addInhibitorArcs() throws FileNotFoundException {
-        String filePath = "example.pnml";
-        String targetSource = "p-614C-28EF-3";
-        String targetTarget = "t-614C-28F3-6";
+    public static void modifyPnml() throws FileNotFoundException {
+        String filePath = System.getenv("fpath") + "/exporttest.pnml";
+        //String targetId = inhibitorArcs.get(0); // <-- укажи нужный id arc
 
-        // Пространство имён PNML
         String namespace = "http://www.pnml.org/version-2009/grammar/pnml";
 
-        // Чтение XML-файла через Axiom
         FileInputStream fis = new FileInputStream(filePath);
         XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-        //StAXOMBuilder builder = new StAXOMBuilder(inputFactory, fis);
         OMXMLParserWrapper builder = OMXMLBuilderFactory.createOMBuilder(fis);
         OMElement root = builder.getDocumentElement();
 
-        // Поиск всех элементов arc
-        for (Iterator<?> it = root.getDescendants(true); it.hasNext(); ) {
-            Object next = it.next();
-            if (next instanceof OMElement) {
-                OMElement elem = (OMElement) next;
-                if ("arc".equals(elem.getLocalName()) &&
-                        namespace.equals(elem.getNamespace().getNamespaceURI())) {
 
-                    String source = elem.getAttributeValue(new QName("source"));
-                    String target = elem.getAttributeValue(new QName("target"));
+        for(String targetId: inhibitorArcs) {
+            // Поиск arc по id
+            for (Iterator<?> it = root.getDescendants(true); it.hasNext(); ) {
+                Object next = it.next();
+                if (next instanceof OMElement) {
+                    OMElement elem = (OMElement) next;
+                    if ("arc".equals(elem.getLocalName()) &&
+                            namespace.equals(elem.getNamespace().getNamespaceURI())) {
 
-                    if (targetSource.equals(source) && targetTarget.equals(target)) {
-                        // Проверяем, нет ли уже <type>
-                        boolean hasType = false;
-                        for (Iterator<?> childIt = elem.getChildElements(); childIt.hasNext(); ) {
-                            OMElement child = (OMElement) childIt.next();
-                            if ("type".equals(child.getLocalName())) {
-                                hasType = true;
-                                break;
+                        String id = elem.getAttributeValue(new QName("id"));
+                        if (targetId.equals(PetriNetBuilder.escapeXml(id))) {
+                            // Проверка, есть ли уже <type>
+                            boolean hasType = false;
+                            for (Iterator<?> childIt = elem.getChildElements(); childIt.hasNext(); ) {
+                                OMElement child = (OMElement) childIt.next();
+                                if ("type".equals(child.getLocalName())) {
+                                    hasType = true;
+                                    break;
+                                }
                             }
-                        }
-                        if (!hasType) {
-                            // Создаём и добавляем <type value="inhibitor"/>
-                            OMFactory factory = elem.getOMFactory();
-                            OMElement type = factory.createOMElement("type", null);
-                            type.addAttribute("value", "inhibitor", null);
-                            elem.addChild(type);
-                            System.out.println("Добавлен <type value=\"inhibitor\"/> к arc " + elem.getAttributeValue(new QName("id")));
+
+                            if (!hasType) {
+                                // Добавляем <type value="inhibitor"/>
+                                OMFactory factory = elem.getOMFactory();
+                                OMElement type = factory.createOMElement("type", null);
+                                type.addAttribute("value", "inhibitor", null);
+                                elem.addChild(type);
+                                System.out.println("Добавлен <type value=\"inhibitor\"/> в arc с id=" + id);
+                            } else {
+                                System.out.println("Элемент <type> уже существует в arc с id=" + id);
+                            }
                         }
                     }
                 }
